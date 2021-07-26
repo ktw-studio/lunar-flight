c3b25d1816373bde0911800109fa4ec5eb7ad228
Marcin Mazurowski @ _KTW-STUDIO <hello@ktwstudio.com>
2021-07-26 22:15:10 +0200

    v0.1.3

--- packages/system/src/system.ts
+++ packages/system/src/system.ts
@@ -2,6 +2,6 @@ import { Procedure } from './procedure';
 
 export class System {
   public async execute(procedures: Array<Procedure<unknown, unknown>>) {
-    await procedures.reduce((prev, curr) => prev.then((output) => curr.run(output)), Promise.resolve());
+    return procedures.reduce((prev, curr) => prev.then((output) => curr.run(output)), Promise.resolve());
   }
 }
